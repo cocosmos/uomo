5b4de2558bec4e39b38996754a6e7f0b3bd94c6c
Cocosmos <mipam.guillot@crea-inseec.com>
2021-05-18 01:08:45 +0200

    Finish 404, faq and terms, location in development map info box works wells also scroll design

--- script/accordion.js
+++ script/accordion.js
@@ -1,15 +1,14 @@
-
-var acc = document.getElementsByClassName("accordion");
+var acc = document.getElementsByClassName("accordion__question");
 var i;
 
 for (i = 0; i < acc.length; i++) {
-    acc[i].addEventListener("click", function() {
-      this.classList.toggle("active");
-      var panel = this.nextElementSibling;
-      if (panel.style.maxHeight) {
-        panel.style.maxHeight = null;
-      } else {
-        panel.style.maxHeight = panel.scrollHeight + "px";
-      } 
-    });
+  acc[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    var accordion__answer = this.nextElementSibling;
+    if (accordion__answer.style.maxHeight) {
+      accordion__answer.style.maxHeight = null;
+    } else {
+      accordion__answer.style.maxHeight = accordion__answer.scrollHeight + "px";
+    } 
+  });
 }
---
+++ script/maps.js
@@ -0,0 +1,77 @@
+  function init() {
+        var i;
+        //Custom Icons
+        const icons = {
+            location: {
+              icon: "../assets/placeholder.png",
+            },
+          };
+          //Custom destinations
+        var arrDestinations = [
+            {
+                lat: 50.815155, 
+                lon: -0.137072, 
+                title: "Store in London",
+                adress: "1418 River Drive, Suite 35 Cottonhall, CA 9622",
+                country: "United States",
+                phone: "+1 246-345-0695",
+                timetable: "10 am - 10 pm EST, 7 days a week",
+                type: "location",
+            },
+            {
+                lat: 50.822638, 
+                lon: -0.137361, 
+                title: "Store in Istanbul",
+                adress: "1418 River Drive, Suite 35 Cottonhall, CA 9622",
+                country: "United States",
+                phone: "+1 246-345-0695",
+                timetable: "10 am - 10 pm EST, 7 days a week",
+                type: "location",
+            },
+            {
+                lat: 50.821226, 
+                lon: -0.139372, 
+                title: "Store in New York",
+                adress: "1418 River Drive, Suite 35 Cottonhall, CA 9622",
+                country: "United States",
+                phone: "+1 246-345-0695",
+                timetable: "10 am - 10 pm EST, 7 days a week",
+                type: "location",
+            }
+        ];
+        
+        var myOptions = {
+            zoom: 13,
+            center: new google.maps.LatLng(50.820645,-0.137376),
+            mapTypeId: google.maps.MapTypeId.ROADMAP,
+        };
+        
+        var map = new google.maps.Map(document.getElementById("map"), myOptions);
+        
+        var infowindow =  new google.maps.InfoWindow({
+            content: ''
+        });
+        
+        // Loop to load every locations
+        for (i = 0; i < arrDestinations.length; i++) {
+            // create a marker for google maps
+            var marker = new google.maps.Marker({
+                title: arrDestinations[i].title,
+                position: new google.maps.LatLng(arrDestinations[i].lat, arrDestinations[i].lon),
+                icon: icons[arrDestinations[i].type].icon,
+                map: map,
+            });
+            
+            // When click on marker it shows information
+            bindInfoWindow(marker, map, infowindow, "<h2>" + arrDestinations[i].title+"</h2>" + "<p>" + arrDestinations[i].adress + "</p>"+ "<p>" + arrDestinations[i].country + "</p>"+ "<p>" + arrDestinations[i].phone + "</p>"+ "<p>" + arrDestinations[i].timetable + "</p>");  
+        }
+    }
+    
+    function bindInfoWindow(marker, map, infowindow, html) { 
+        google.maps.event.addListener(marker, 'click', function() { 
+            infowindow.setContent(html); 
+            infowindow.open(map, marker); 
+        }); 
+    } 
+    
+    google.maps.event.addDomListener(window, 'load', init);
